97a013ae767d8419f9f2710b34bd8126fc718428
Vitaliy <mvit_svk@mail.ru>
2021-04-12 15:37:59 +0300

    NFS ready

---
+++ src/com/gb/nfs/Stage.java
@@ -0,0 +1,13 @@
+package com.gb.nfs;
+
+import java.util.concurrent.Semaphore;
+
+public abstract class Stage {
+    protected int length;
+    protected String description;
+
+    public String getDescription() {
+        return description;
+    }
+    public abstract void go(Car c, Semaphore semaphore);
+}
